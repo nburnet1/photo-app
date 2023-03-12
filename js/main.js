27add58a855f717953c07f5ea0122ef3f27b189b
nburnet1 <nburnet1@unca.edu>
2023-03-12 01:26:47 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -226,7 +226,7 @@ const suggestionsToHTML = (data) => {
                             <h3><b>${data.username}</b></h3>
                             <p>Suggested For You</p>
                         </li>
-                        <li class="follow">
+                        <li id="class_follow_${data.id}"class="follow">
                             ${checkFollow(data)}
                         </li>
     </ul>
@@ -235,7 +235,7 @@ const suggestionsToHTML = (data) => {
 
 const postToHTML = (data) => {
     return `
-    <section class="card">
+    <section id="card_${data.id}" class="card">
                 <ul class="post">
                     <li class="post-head">
                         <h4 class="post-user">
@@ -273,7 +273,7 @@ const postToHTML = (data) => {
                     <div class="post-date">
                     ${data.display_time}
                     </div>
-                    <li class="comments">
+                    <li id="comment_list_${data.id}" class="comments">
                         
                         ${checkComments(data)}
 
@@ -315,24 +315,103 @@ const postToHTML = (data) => {
 
 }
 
+const repostToHTML = data =>{
+    return `
+    <ul class="post">
+                    <li class="post-head">
+                        <h4 class="post-user">
+                            ${data.user.username}
+                        </h4>
+                        <a href=""><i class="fa-solid fa-ellipsis"></i></a>
+                    </li>
+                    
+                    <li class="pic-post">
+                        <img class="post-img" src="${data.image_url}" />
+                    </li>
+                    <li class="icons-list">
+                        <div class="interact-list">
+                            ${checkHeart(data)}
+                            <button class="fa-regular fa-comment"></button>
+                            <button class="fa-regular fa-paper-plane"></button>
+                        </div>
+                        <div>
+                            ${checkBookmark(data)}
+                        </div>
+                        
+                    </li>
+                    <li class="likes" >
+                        <div id="likes_${data.id}">
+                        <b>${data.likes.length} Likes</b>
+                        </div>
+                    </li>
+                    <li class="post-des">
+                        <div class="post-user"><a href=""><b>${data.user.username}</b></a></div>
+                        <div class="description">
+                        ${data.caption}
+                        </div>
+                        
+                    </li>
+                    <div class="post-date">
+                    ${data.display_time}
+                    </div>
+                    <li id="comment_list_${data.id}" class="comments">
+                        
+                        ${checkComments(data)}
+
+
+                    </li>
+                    <li class="post-footer">
+                        <div class="add-comment">
+                            <input id="comment_${data.id}" placeholder="Add a comment..."/>
+                        </div>
+                        <button onclick="updateComment(id)" id="post_${data.id}" >Post</button>
+                    </li>
+                </ul>
+                <div id="modal${data.id}" class="modal-bg hidden" aria-hidden="true" >
+                        <div class="modal-post">
+                        <button id="${data.id}" class="close-modal fa-solid fa-x"></button>
+                            <img class="modal-img" src="${data.image_url}"/>
+
+                            <div class="modal-text">
+                                <div class="modal-des">
+                                    <ul class="user-section">
+                                        <li class="logged-img">
+                                            <img class="user-pfp" src="${data.user.thumb_url}"/>
+                                        </li>
+                                        <li class="logged-user">
+                                            <b><h2>${data.user.username}</h2></b>
+                                        </li>
+                                    
+                                    </ul>
+                                </div>
+                                <div class="modal-comments">
+                                    ${showModalComments(data)}
+                                </div>
+                            </div>
+                        </div>
+                    
+                </div>
+    `;
+}
+
 
 // Check Functions
 const checkHeart = data =>{
 
     if(typeof data.current_user_like_id == 'undefined'){
-        return `<button id="heart_${data.id}" onclick="postHeart(id)" class="fa-regular fa-heart"></button>`;
+        return `<button aria-label="Heart" aria-checked="false" id="heart_${data.id}" onclick="postHeart(id)" class="fa-regular fa-heart"></button>`;
     }
     else{
-        return `<button id="heart_${data.id}_${data.current_user_like_id}" onclick="deleteHeart(id)" style="color:red;" class="fa-solid fa-heart"></button>`;
+        return `<button aria-label="Heart" aria-label="true" id="heart_${data.id}_${data.current_user_like_id}" onclick="deleteHeart(id)" style="color:red;" class="fa-solid fa-heart"></button>`;
     }
 
 }
 const checkBookmark = data =>{
     if(typeof data.current_user_bookmark_id == 'undefined'){
-        return `<button id="bookmark_${data.id}" onclick="postBookmark(id)" class="fa-regular fa-bookmark"></button>`;
+        return `<button aria-label="Bookmark" aria-checked="false" id="bookmark_${data.id}" onclick="postBookmark(id)" class="fa-regular fa-bookmark"></button>`;
     }
     else{
-        return `<button id="bookmark_${data.id}_${data.current_user_bookmark_id}" onclick="deleteBookmark(id)" class="fa-solid fa-bookmark"></button>`;
+        return `<button aria-label="Bookmark" aria-checked="true" id="bookmark_${data.id}_${data.current_user_bookmark_id}" onclick="deleteBookmark(id)" class="fa-solid fa-bookmark"></button>`;
     }
 }
 const checkComments = data =>{
@@ -367,11 +446,11 @@ const checkComments = data =>{
 
 const checkFollow = (data) =>{
     return `
-    <button id="follow_${data.id}" onclick="postFollow(id)">Follow</button>
+    <button aria-label="Follow" aria-checked="false" id="follow_${data.id}" onclick="postFollow(id)">Follow</button>
     `
 }
 
-// delete functions
+// post functions
 
 window.postHeart = async (id) =>{
     console.log("Liking");
@@ -443,19 +522,45 @@ window.postFollow = async (id) =>{
     })
     const data = await response.json();
     console.log("Redraw Data Follow: ",data);
-    redrawFollow(id);
+    redrawFollow(id,data);
 }
 
-window.deleteFollow = async id =>{
-    const data = showFollowing();
+window.updateComment = async id =>{
 
-    const userID = id.split("_");
+    const post = document.getElementById(id);
+    const comment = document.getElementById(`comment_${id.split("_")[1]}`).value;
+    const postID = id.split("_")[1]
+
+
+    const endpoint = `${rootURL}/api/comments/`;
+
+    const postData = {
+        "post_id": postID,
+        "text": comment
+    };
+    
+    const response = await fetch(endpoint, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + await getAccessToken(rootURL, 'noah', 'noah_password')
+        },
+        body: JSON.stringify(postData)
+    })
+    const data = await response.json();
 
     console.log(data);
 
-}
+    redrawComment(data);
 
-// post functions
+
+
+
+} 
+
+
+
+// delete functions
 
 window.deleteHeart = async (id) =>{
     console.log("Deleting Like");
@@ -497,8 +602,30 @@ window.deleteBookmark = async (id) =>{
     })
 
     const data = await response.json();
-    redrawBookmark(postID[1],postID[2],1)
+    redrawBookmark(postID[1],postID[2],1);
     
+}
+
+window.deleteFollow = async (id) =>{
+
+    const orgID = id.split("_")[0]+"_"+id.split("_")[1]
+
+    console.log("Unfollowing");
+
+    console.log(id,orgID);
+
+    const endpoint = `${rootURL}/api/following/${id.split("_")[2]}`;
+
+    // Create the bookmark:
+    const response = await fetch(endpoint, {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + await getAccessToken(rootURL, 'noah', 'noah_password')
+        }
+    });
+    redrawUnfollow(orgID);
+
 }
 
 // redraw functions
@@ -565,13 +692,37 @@ const redrawBookmark = async (id,bookmarkID,del) =>{
     }
 }
 
-const redrawFollow = async (id) =>{
-    console.log("redrawing");
+const redrawFollow =(id,data) =>{
+    console.log("redrawing",id,data);
 
-    document.getElementById(id).innerHTML = `
-        <button id="${id}" onclick="deleteFollow(id)">unfollow</button>
+    document.getElementById("class_"+id).innerHTML = `
+        <button aria-label="Follow" aria-checked="true" id="${id+"_"+data.id}" onclick="deleteFollow(id)">unfollow</button>
+    `;
+}
+
+const redrawUnfollow = (id) =>{
+    console.log("redrawing follow: ", id);
+
+    document.getElementById("class_"+id).innerHTML = `
+        <button aria-label="Follow" aria-checked="false" id="${id}" onclick="postFollow(id)">follow</button>
     `;
 
+}
+
+const redrawComment = async datum =>{
+    const endpoint = `${rootURL}/api/posts/${datum.post_id}`;
+    const response = await fetch(endpoint, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + await getAccessToken(rootURL, 'noah', 'noah_password')
+        }
+    })
+    const data = await response.json();
+    const htmlString = postToHTML(data);
+
+    targetElementAndReplace(`card_${data.id}`, htmlString);
+
+    document.getElementById(`comment_${data.id}`).focus()
 
 }
 
